870f4ee5ed54e77ba7463f5d058e1209c28ccc15
b10619038 <b10619038@users.noreply.github.com>
2026-08-28 18:31:40 +0000

    凌晨2:32：後台首頁預覽五層四戶透天模型

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v108";
-const BUILD = "20260829-0218";
+const CACHE = "tongjie-app-v109";
+const BUILD = "20260829-0232";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
