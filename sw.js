263dfa6587fc3856003848b7aea4a0dd4fc81785
b10619038 <b10619038@users.noreply.github.com>
2026-08-28 17:20:20 +0000

    凌晨1:40：套入115年7月進出帳與期初餘額

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v94";
-const BUILD = "20260829-0135";
+const CACHE = "tongjie-app-v95";
+const BUILD = "20260829-0140";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
