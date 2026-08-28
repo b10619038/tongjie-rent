b12e453093268fe2ac519fc80524b59a12a4955f
b10619038 <b10619038@users.noreply.github.com>
2026-08-28 17:25:26 +0000

    凌晨1:42：修復進入空白（7月帳資料改到啟動前）

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v95";
-const BUILD = "20260829-0140";
+const CACHE = "tongjie-app-v96";
+const BUILD = "20260829-0142";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
