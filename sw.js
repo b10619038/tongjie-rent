d6a300e50a1acfccab32e0bf17609a52c7f75003
b10619038 <b10619038@users.noreply.github.com>
2026-08-28 18:03:47 +0000

    凌晨2:10：收租率與出租率圓餅改玫瑰與金黃

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v103";
-const BUILD = "20260829-0208";
+const CACHE = "tongjie-app-v104";
+const BUILD = "20260829-0210";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
