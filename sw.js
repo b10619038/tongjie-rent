3be293a3829e06a66fa637755c6542def27cdf41
b10619038 <b10619038@users.noreply.github.com>
2026-08-30 12:53:54 +0000

    晚上20:53：開發者工作助手改用同一張頭貼

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v310";
-const BUILD = "20260830-2052";
+const CACHE = "tongjie-app-v311";
+const BUILD = "20260830-2053";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
