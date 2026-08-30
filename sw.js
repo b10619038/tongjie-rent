3716c6774797230e7d7d056788f8b13628fed854
b10619038 <b10619038@users.noreply.github.com>
2026-08-30 13:04:50 +0000

    晚上21:04：更新快取讓震動生效

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v315";
-const BUILD = "20260830-2103";
+const CACHE = "tongjie-app-v316";
+const BUILD = "20260830-2104";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
