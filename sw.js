2133e46468797c9d71af86ced24869b3914925c8
b10619038 <b10619038@users.noreply.github.com>
2026-08-30 17:38:34 +0000

    凌晨01:38：統編紅字右移0.5公分下移0.2公分

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v367";
-const BUILD = "20260831-0134";
+const CACHE = "tongjie-app-v368";
+const BUILD = "20260831-0138";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
