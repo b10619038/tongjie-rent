5406cda646690244e01c813108ca3e5725aa65ca
b10619038 <b10619038@users.noreply.github.com>
2026-08-30 17:09:35 +0000

    凌晨01:09：備註房號往下移一點

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v357";
-const BUILD = "20260831-0108";
+const CACHE = "tongjie-app-v358";
+const BUILD = "20260831-0109";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
