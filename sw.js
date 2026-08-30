051b1115e1d090c4060333021e90858a5aee161b
b10619038 <b10619038@users.noreply.github.com>
2026-08-30 17:06:39 +0000

    凌晨01:07：應稅V移到稅字右邊空格

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v355";
-const BUILD = "20260831-0106";
+const CACHE = "tongjie-app-v356";
+const BUILD = "20260831-0107";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
