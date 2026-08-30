32268f87a44d0e28a9abfbc7b214457176a5909e
b10619038 <b10619038@users.noreply.github.com>
2026-08-30 05:54:41 +0000

    下午13:54：統潔帳戶圖卡移除超商

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v286";
-const BUILD = "20260830-1352";
+const CACHE = "tongjie-app-v287";
+const BUILD = "20260830-1354";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
