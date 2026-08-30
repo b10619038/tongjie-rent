e6024433f1f98bb68b21437b67af50c05d4d19ab
b10619038 <b10619038@users.noreply.github.com>
2026-08-30 17:47:49 +0000

    凌晨01:49：三聯數量一式往右0.3公分

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v375";
-const BUILD = "20260831-0147";
+const CACHE = "tongjie-app-v376";
+const BUILD = "20260831-0149";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
