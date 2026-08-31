4d7b513745fbf91b9bce04f0cab5be16c72172c3
b10619038 <b10619038@users.noreply.github.com>
2026-08-31 08:36:18 +0000

    下午16:25：灌入115年7月對帳總表並核對期末餘額

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v399";
-const BUILD = "20260831-1615";
+const CACHE = "tongjie-app-v400";
+const BUILD = "20260831-1625";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
