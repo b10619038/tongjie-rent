c8bc267ff2f5ff2ea97342d97ae0d3af21a0892a
b10619038 <b10619038@users.noreply.github.com>
2026-08-31 18:10:31 +0000

    凌晨02:10：早安美芝城改用你提供的菜單照片

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const ACCOUNT_BANKS = { "統潔": ["聯邦", "農會", "兆豐"], "信潔": ["�
 const BANK_PLACES = ["聯邦", "兆豐", "農會", "超商"];
 const PERSONAL_PEOPLE = ["趙文榮", "趙洪漳", "趙浩鈞", "趙文彬", "趙苡真", "趙海成、趙正賢", "江秀霞", "黃思敏"];
 const PERSONAL_ACCOUNTS = PERSONAL_PEOPLE.map(p => "個人戶·" + p);
-const APP_STAMP = "2026-09-01-02-08";
-const APP_EDIT_COUNT = 403;
+const APP_STAMP = "2026-09-01-02-10";
+const APP_EDIT_COUNT = 404;
 const RENT_DUE_DAY = 1;
 const DUE_DAY_VER = "due1-v1";
 const DOCS_IMPORT_VER = "aug31docs-v1";
@@ -54,7 +54,7 @@ const TENANT_ROSTER_VER = "20260831-2120";
 const FACTORY_ROSTER_VER = "20260831-1710";
 const STUDIO_FEE_VER = "20260831-2120";
 const CHANGELOG = [
-  { ver: APP_STAMP, items: ["常用店家加上菜單照片，點開可放大"] },
+  { ver: APP_STAMP, items: ["早安美芝城改用你提供的菜單照片"] },
   { ver: "2026-08-31-13-56", items: ["公司門禁新增辦公室門鎖並移除複製"] },
   { ver: "2026-08-31-13-53", items: ["公司門禁加上 M3F 密碼鎖說明"] },
   { ver: "2026-08-31-13-52", items: ["設定新增公司門禁密碼"] },
@@ -8518,7 +8518,7 @@ const DEFAULT_LUNCH_SPOTS = [
     hours: "每日 06:00–12:50",
     menu: ["里肌豬排吐司 79", "鮪魚沙拉蛋餅 65", "卡啦辣雞腿堡 99", "阿拉斯加鱈魚堡 90"],
     note: "文龍東路，可電話預訂",
-    photos: ["images/food/macc.jpg"]
+    photos: ["images/food/macc.jpg?v=0209"]
   },
   {
     id: "laya", name: "拉亞漢堡 鳳山北文店", near: "走路約 5 分鐘", tags: ["早午餐", "飲料"],
