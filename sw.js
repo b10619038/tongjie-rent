bb366700b5ad484ad33e2a08e4915a6091538701
b10619038 <b10619038@users.noreply.github.com>
2026-09-01 08:06:48 +0000

    下午4:12：7月歷史明細已套入總覽（含牛案場）

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v493";
-const BUILD = "20260901-1206";
+const CACHE = "tongjie-app-v494";
+const BUILD = "20260901-1612";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
