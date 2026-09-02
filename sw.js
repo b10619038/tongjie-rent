de238d49e9328e635d769072ebc8ee4590e923c4
b10619038 <b10619038@users.noreply.github.com>
2026-09-02 14:27:04 +0000

    晚上10:30：合約起迄標題加上一年，登入頁也會跳出更新

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v613";
-const BUILD = "20260902-0097";
+const CACHE = "tongjie-app-v614";
+const BUILD = "20260902-0098";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
