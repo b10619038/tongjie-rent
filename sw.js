8dc3b7373cc715a78c29f34b2a801c4a1e90c6c0
b10619038 <b10619038@users.noreply.github.com>
2026-09-02 10:26:29 +0000

    下午6:27：廠房租客搜尋列改成搜尋

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v583";
-const BUILD = "20260902-0067";
+const CACHE = "tongjie-app-v584";
+const BUILD = "20260902-0068";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
