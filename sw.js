3ec6067258727f50bbd5ebd51aa0dd021ef461fa
b10619038 <b10619038@users.noreply.github.com>
2026-09-02 08:03:09 +0000

    下午4:03：發票匯款日以該月1日為準

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v560";
-const BUILD = "20260902-0044";
+const CACHE = "tongjie-app-v561";
+const BUILD = "20260902-0045";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
