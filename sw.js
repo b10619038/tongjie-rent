7eac5e850cb6edbeac46b1b73342a21b8f9aed9b
tongjie <b10619038@tongjie.app>
2026-09-04 09:06:53 +0000

    下午5:10：房屋租賃契約書每一面改回滿版

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v741";
-const BUILD = "20260904-0225";
+const CACHE = "tongjie-app-v742";
+const BUILD = "20260904-0226";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
