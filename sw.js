ef10cb14f0a9787e1754db9b9bca919fa3ef4789
tongjie <b10619038@tongjie.app>
2026-09-04 05:23:09 +0000

    下午1:22：租客畫面房間圖示改成開門

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v718";
-const BUILD = "20260904-0202";
+const CACHE = "tongjie-app-v719";
+const BUILD = "20260904-0203";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
