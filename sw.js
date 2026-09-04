8a812ad0859facf6c1bc780a2918f683526bf26e
tongjie <b10619038@tongjie.app>
2026-09-04 09:01:55 +0000

    下午5:06：回報已繳費並附上截圖改成綠色可按按鈕

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v740";
-const BUILD = "20260904-0224";
+const CACHE = "tongjie-app-v741";
+const BUILD = "20260904-0225";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
