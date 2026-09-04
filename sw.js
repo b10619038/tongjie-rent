5a32b9302f8d8282a2792ffc36f8e46d2d9c33bc
tongjie <b10619038@tongjie.app>
2026-09-04 08:41:17 +0000

    下午4:46：租客繳費官方 LINE 通知改到本月已繳費上方

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v736";
-const BUILD = "20260904-0220";
+const CACHE = "tongjie-app-v737";
+const BUILD = "20260904-0221";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
