ad421827ae4b790f215c355406d47c221f358474
tongjie <b10619038@tongjie.app>
2026-09-04 02:04:41 +0000

    上午10:05：登入說明改成破折號管理平台

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v675";
-const BUILD = "20260904-0159";
+const CACHE = "tongjie-app-v676";
+const BUILD = "20260904-0160";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
