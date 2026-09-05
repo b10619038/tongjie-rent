53ecaffaaee62c4c2df37ca2bb11f0af4ce58b37
tongjie <b10619038@tongjie.app>
2026-09-05 09:28:44 +0000

    下午5:27：排法改回，只把浮動球往右靠

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v804";
-const BUILD = "20260905-0288";
+const CACHE = "tongjie-app-v805";
+const BUILD = "20260905-0289";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
