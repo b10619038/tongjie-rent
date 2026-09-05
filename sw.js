cb4847680752646f0259702bd8d23cbe995806e9
tongjie <b10619038@tongjie.app>
2026-09-05 13:36:50 +0000

    下午9:36：報修類型與刪除報修輕點回彈

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v848";
-const BUILD = "20260905-0332";
+const CACHE = "tongjie-app-v849";
+const BUILD = "20260905-0333";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
