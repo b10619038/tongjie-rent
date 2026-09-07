fe6096097ba2614857422e7effa56d081783ceef
tongjie <b10619038@tongjie.app>
2026-09-07 14:38:39 +0000

    晚上10:37：統潔聯邦金額圖塊左右縮窄

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ const ACCOUNT_BANKS = { "統潔": ["聯邦", "農會", "兆豐"], "信潔": ["�
 const BANK_PLACES = ["聯邦", "兆豐", "農會", "超商"];
 const PERSONAL_PEOPLE = ["趙文榮", "趙洪漳", "趙浩鈞", "趙文彬", "趙苡真", "趙海成、趙正賢", "趙貴美", "江秀霞", "黃思敏"];
 const PERSONAL_ACCOUNTS = PERSONAL_PEOPLE.map(p => "個人戶·" + p);
-const APP_STAMP = "2026-09-07-22-34";
-const APP_EDIT_COUNT = 816;
+const APP_STAMP = "2026-09-07-22-37";
+const APP_EDIT_COUNT = 817;
 const APP_VERSION = APP_STAMP + "-" + String(APP_EDIT_COUNT);
-const FILE_VER = "0366";
+const FILE_VER = "0367";
 const BOOK_UP_BLOBS = Object.create(null);
 const RENT_DUE_DAY = 1;
 const DUE_DAY_VER = "due1-v1";
@@ -89,7 +89,8 @@ const FACTORY_ROSTER_VER = "20260902-1920";
 const FACTORY_PAID_RESET_VER = "20260902-1258";
 const STUDIO_FEE_VER = "20260831-2120";
 const CHANGELOG = [
-  { ver: APP_VERSION, items: ["業務上傳帳戶和金額圖塊改矮一點"] },
+  { ver: APP_VERSION, items: ["業務上傳帳戶和金額圖塊左右縮窄"] },
+  { ver: "2026-09-07-22-34-816", items: ["業務上傳帳戶和金額圖塊改矮一點"] },
   { ver: "2026-09-07-22-29-815", items: ["浮動球不會再飛出畫面，可改回圖塊"] },
   { ver: "2026-09-07-22-27-814", items: ["業務上傳進出轉帳字改置中"] },
   { ver: "2026-09-07-22-25-813", items: ["93-1B與93-2A共用電單，總金額除總度數再乘各戶度數開紅單"] },
