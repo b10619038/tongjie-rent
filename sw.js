d2282ecb12e6fbf615b85d6110ad7050af39efa1
tongjie <b10619038@tongjie.app>
2026-09-09 12:42:57 +0000

    晚上9:00：本月工作移除繳勞健保

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v925";
-const BUILD = "20260909-0409";
+const CACHE = "tongjie-app-v926";
+const BUILD = "20260909-0410";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
