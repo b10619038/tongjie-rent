bb50e9bec54b76eada287d0526e23eee33f365b8
tongjie <b10619038@tongjie.app>
2026-09-09 13:13:01 +0000

    晚上9:40：平面改依手繪總圖街廓

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v929";
-const BUILD = "20260909-0413";
+const CACHE = "tongjie-app-v930";
+const BUILD = "20260909-0414";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
