3d0c8b9975da17942ccb44969691566a5c0d3b4d
tongjie <b10619038@tongjie.app>
2026-09-10 14:58:25 +0000

    晚上11:30：太陽能圓餅圖改到右邊

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v951";
-const BUILD = "20260910-0435";
+const CACHE = "tongjie-app-v952";
+const BUILD = "20260910-0436";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
