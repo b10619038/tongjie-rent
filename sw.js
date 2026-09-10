486ce2eb9ca6d2ad6fade68fb1855b7f67eb9823
tongjie <b10619038@tongjie.app>
2026-09-10 15:51:35 +0000

    凌晨12:32：上方分頁滑鼠移過去不再反白

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v958";
-const BUILD = "20260911-0442";
+const CACHE = "tongjie-app-v959";
+const BUILD = "20260911-0443";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
