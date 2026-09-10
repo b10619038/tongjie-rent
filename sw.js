46323a908ca1dacdcef68abded0fd2d197dcd925
tongjie <b10619038@tongjie.app>
2026-09-10 15:44:44 +0000

    凌晨12:20：上方分頁改成輕點回饋，拿掉滑過去

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v955";
-const BUILD = "20260910-0439";
+const CACHE = "tongjie-app-v956";
+const BUILD = "20260911-0440";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
