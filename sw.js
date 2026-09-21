8b3d24bf2e22607c99d0f1d162f45593faf40552
tongjie <b10619038@tongjie.app>
2026-09-21 16:34:08 +0000

    晚上12:38：續約申請按鈕按完會彈回，不再卡縮小

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1042";
-const BUILD = "20260922-0527";
+const CACHE = "tongjie-app-v1043";
+const BUILD = "20260922-0528";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
