05be522eb4657f49aa5cac128d2da3c2a1b974fb
tongjie <b10619038@tongjie.app>
2026-09-21 15:46:23 +0000

    晚上11:48：續約申請縮放改成和空套房／已繳／未繳同一手感

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1032";
-const BUILD = "20260921-0517";
+const CACHE = "tongjie-app-v1033";
+const BUILD = "20260921-0518";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
