2912e826a706f45c68573a427c4454ee4647604f
tongjie <b10619038@tongjie.app>
2026-09-21 15:18:18 +0000

    晚上11:23：旁邊那格改為拉皮 93-2A

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1027";
-const BUILD = "20260921-0512";
+const CACHE = "tongjie-app-v1028";
+const BUILD = "20260921-0513";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
