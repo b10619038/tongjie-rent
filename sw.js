8bad50379873988b8b951e1b6ffaae4c76a85e1a
tongjie <b10619038@tongjie.app>
2026-09-21 16:16:23 +0000

    晚上12:18：續約申請租客圖卡改和其他人一樣，只留左上紅點

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1037";
-const BUILD = "20260922-0522";
+const CACHE = "tongjie-app-v1038";
+const BUILD = "20260922-0523";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
