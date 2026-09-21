a4d33792b665aeae36ce03f6d871ddae50e2a0b6
tongjie <b10619038@tongjie.app>
2026-09-21 13:03:16 +0000

    晚上9:04：續約申請按鈕拿掉邊線

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v995";
-const BUILD = "20260921-0480";
+const CACHE = "tongjie-app-v996";
+const BUILD = "20260921-0481";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
