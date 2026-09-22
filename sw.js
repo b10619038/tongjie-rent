3f071a3d5e138f34b6380edb5865bed518f7c40f
tongjie <b10619038@tongjie.app>
2026-09-22 06:28:57 +0000

    下午2:28：7032 續約姓名往右移 0.3cm

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1068";
-const BUILD = "20260922-0553";
+const CACHE = "tongjie-app-v1069";
+const BUILD = "20260922-0554";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
