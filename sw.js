cd0e8900987306c5bc6474f83ea2d8a5dc69a028
tongjie <b10619038@tongjie.app>
2026-09-22 16:25:34 +0000

    上午12:25：辦理退租選單不再破圖

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1174";
-const BUILD = "20260923-0659";
+const CACHE = "tongjie-app-v1175";
+const BUILD = "20260923-0660";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
