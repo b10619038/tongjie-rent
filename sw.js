72d2b7c641a825e4c56237045db209f9b17435cf
tongjie <b10619038@tongjie.app>
2026-09-22 14:07:08 +0000

    下午10:07：後台首頁移除邀請加入與開發者對話圖卡

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1158";
-const BUILD = "20260922-0643";
+const CACHE = "tongjie-app-v1159";
+const BUILD = "20260922-0644";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
