b372933caf91851910140e808a014b0e8e727592
tongjie <b10619038@tongjie.app>
2026-09-22 13:52:24 +0000

    下午9:52：本月已繳印章改用不透明度蓋章，不再用黑影

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1154";
-const BUILD = "20260922-0639";
+const CACHE = "tongjie-app-v1155";
+const BUILD = "20260922-0640";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
