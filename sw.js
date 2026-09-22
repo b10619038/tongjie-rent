cd0e3fd5cab7f44961557bd065cac3994e66185b
tongjie <b10619038@tongjie.app>
2026-09-22 08:39:36 +0000

    下午4:39：本月已入帳圖卡的產出發票可點

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1097";
-const BUILD = "20260922-0582";
+const CACHE = "tongjie-app-v1098";
+const BUILD = "20260922-0583";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
