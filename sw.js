942c0dca7c6b8f1f1ae5567ddc3e95b0a986aadd
tongjie <b10619038@tongjie.app>
2026-09-22 15:23:24 +0000

    下午11:24：花盆可選十種形狀與十種顏色

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1165";
-const BUILD = "20260922-0650";
+const CACHE = "tongjie-app-v1166";
+const BUILD = "20260922-0651";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
