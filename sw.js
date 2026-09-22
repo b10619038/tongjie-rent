6eb660b15be88cd47e819ee43a78a86734f76ec8
tongjie <b10619038@tongjie.app>
2026-09-22 09:18:09 +0000

    下午5:18：續約完成後剩餘天數先顯示 +新約天數，新約第一天起改回倒數

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1101";
-const BUILD = "20260922-0586";
+const CACHE = "tongjie-app-v1102";
+const BUILD = "20260922-0587";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
