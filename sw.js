69a3661e126911b44b8a7ba74668a8dd18a36302
tongjie <b10619038@tongjie.app>
2026-09-22 09:59:48 +0000

    下午6:00：本月已入帳圖卡改白底淺灰框

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1108";
-const BUILD = "20260922-0593";
+const CACHE = "tongjie-app-v1109";
+const BUILD = "20260922-0594";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
