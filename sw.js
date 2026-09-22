20fcab680a61de323ac53ca2a8ff0601b26b7b1c
tongjie <b10619038@tongjie.app>
2026-09-22 13:55:20 +0000

    下午9:55：本月已繳印章改等圖卡滑到位再蓋

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1155";
-const BUILD = "20260922-0640";
+const CACHE = "tongjie-app-v1156";
+const BUILD = "20260922-0641";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
