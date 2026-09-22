83b9804f4f9820ccd129f90d5ae863d487d29100
tongjie <b10619038@tongjie.app>
2026-09-22 12:13:15 +0000

    下午8:13：本月已繳印章改成淡黑陰影轉綠壓實

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1123";
-const BUILD = "20260922-0608";
+const CACHE = "tongjie-app-v1124";
+const BUILD = "20260922-0609";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
