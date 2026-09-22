5a662bfc84f640992ded23c2d70e63be76214d08
tongjie <b10619038@tongjie.app>
2026-09-22 09:21:02 +0000

    下午5:21：續約 +天數改同一行，並和本月租金金額切齊

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1102";
-const BUILD = "20260922-0587";
+const CACHE = "tongjie-app-v1103";
+const BUILD = "20260922-0588";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
