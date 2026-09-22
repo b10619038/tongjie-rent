8aeda1c437c8f50b67b6dfcd24a9c666ba60a701
tongjie <b10619038@tongjie.app>
2026-09-22 06:36:59 +0000

    下午2:37：鳳山區農會銀行代號改為 619

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ const ACCOUNT_BANKS = { "統潔": ["聯邦", "農會", "兆豐"], "信潔": ["�
 const BANK_PLACES = ["聯邦", "兆豐", "農會", "超商"];
 const PERSONAL_PEOPLE = ["趙文榮", "趙洪漳", "趙浩鈞", "趙文彬", "趙苡真", "趙海成、趙正賢", "趙貴美", "江秀霞", "黃思敏", "趙淑芬", "許喻涵"];
 const PERSONAL_ACCOUNTS = PERSONAL_PEOPLE.map(p => "個人戶·" + p);
-const APP_STAMP = "2026-09-22-14-36";
-const APP_EDIT_COUNT = 1006;
+const APP_STAMP = "2026-09-22-14-37";
+const APP_EDIT_COUNT = 1007;
 const APP_VERSION = APP_STAMP + "-" + String(APP_EDIT_COUNT);
-const FILE_VER = "0556";
+const FILE_VER = "0557";
 const BOOK_UP_BLOBS = Object.create(null);
 const RENT_DUE_DAY = 1;
 const DUE_DAY_VER = "due1-v1";
@@ -490,7 +490,7 @@ const FACTORY_ROSTER_VER = "20260915-xuxu2";
 const FACTORY_PAID_RESET_VER = "20260902-1258";
 const STUDIO_FEE_VER = "20260831-2120";
 const CHANGELOG = [
-  { ver: APP_VERSION, items: ["繳費頁拿掉客服電話"] },
+  { ver: APP_VERSION, items: ["鳳山區農會銀行代號改為 619"] },
   { ver: "2026-09-21-20-32-926", items: ["續約現場收年水費 1,800 只收現金；7221 張智傑已送出續約申請"] },
   { ver: "2026-09-21-20-08-925", items: ["有新版本改只出現一次，開著 App 不再同時跳出系統通知"] },
   { ver: "2026-09-21-19-58-924", items: ["9/21 錦芳工程款 14,000 現金入保險箱"] },
@@ -1916,7 +1916,7 @@ function stampPlaceOf(r) {
 const COMPANY_BANKS = [
   { company: "統潔", bank: "聯邦銀行", code: "803", account: "010100035909", holder: "統潔開發有限公司", key: "聯邦" },
   { company: "統潔", bank: "聯邦銀行支存", code: "803", account: "010300019225", holder: "統潔開發有限公司", key: "聯邦支存" },
-  { company: "統潔", bank: "鳳山區農會", code: "6190897", account: "00897210912150", holder: "統潔開發有限公司", key: "農會", note: "套房舊客匯款" },
+  { company: "統潔", bank: "鳳山區農會", code: "619", account: "00897210912150", holder: "統潔開發有限公司", key: "農會", note: "套房舊客匯款" },
   { company: "統潔", bank: "兆豐銀行", code: "017", account: "04009039686", holder: "統潔開發有限公司", key: "兆豐", note: "套房新客匯款" },
   { company: "信潔", bank: "聯邦銀行", code: "803", account: "010100034775", holder: "信潔開發有限公司", key: "聯邦" }
 ];
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1071";
-const BUILD = "20260922-0556";
+const CACHE = "tongjie-app-v1072";
+const BUILD = "20260922-0557";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
