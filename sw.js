c484a3353a3a03d25effde5ec201573eee2f8e4f
tongjie <b10619038@tongjie.app>
2026-09-22 16:25:45 +0000

    上午12:26：補回退租表單樣式

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1175";
-const BUILD = "20260923-0660";
+const CACHE = "tongjie-app-v1176";
+const BUILD = "20260923-0661";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
