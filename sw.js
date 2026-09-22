5544726b8ea5b49b5e41e77ed614b5e64b773cb0
tongjie <b10619038@tongjie.app>
2026-09-22 13:15:24 +0000

    下午9:15：租客有頭貼時，對話氣泡左邊會顯示

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1143";
-const BUILD = "20260922-0628";
+const CACHE = "tongjie-app-v1144";
+const BUILD = "20260922-0629";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
