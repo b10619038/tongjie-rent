9bf9fadb2e527053b713cb9db1c732f039cbef05
tongjie <b10619038@tongjie.app>
2026-09-22 08:36:48 +0000

    下午4:37：本月已繳點開後不要外框

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1096";
-const BUILD = "20260922-0581";
+const CACHE = "tongjie-app-v1097";
+const BUILD = "20260922-0582";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
