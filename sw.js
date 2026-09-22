51982ff7f2d49911cd40029c50903005ea077c14
tongjie <b10619038@tongjie.app>
2026-09-22 12:15:49 +0000

    下午8:16：蓋章先出淡黑陰影再顯影成綠色印章

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1124";
-const BUILD = "20260922-0609";
+const CACHE = "tongjie-app-v1125";
+const BUILD = "20260922-0610";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
