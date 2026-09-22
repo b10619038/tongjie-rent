875cbb1110307b9bfae16bcc3192e91a0a76b3ac
tongjie <b10619038@tongjie.app>
2026-09-22 12:07:19 +0000

    下午8:07：本月已繳蓋章動畫加大落下與撞擊

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1121";
-const BUILD = "20260922-0606";
+const CACHE = "tongjie-app-v1122";
+const BUILD = "20260922-0607";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
