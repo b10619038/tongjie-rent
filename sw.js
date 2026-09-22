9ea4d0c9cf30b1f89ba62b3b7df122f7b83d2af3
tongjie <b10619038@tongjie.app>
2026-09-22 13:41:49 +0000

    下午9:40：租客首頁不再閃過要簽約圖塊

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1149";
-const BUILD = "20260922-0634";
+const CACHE = "tongjie-app-v1150";
+const BUILD = "20260922-0635";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
