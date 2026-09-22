603900e2e5508e00d26252375a65d8bfd39b4357
tongjie <b10619038@tongjie.app>
2026-09-22 12:23:28 +0000

    下午8:23：蓋章空白改為 0.3 秒再出黑影

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1126";
-const BUILD = "20260922-0611";
+const CACHE = "tongjie-app-v1127";
+const BUILD = "20260922-0612";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
