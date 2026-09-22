71f3b0176f680f556994da795ea100b5401edc97
tongjie <b10619038@tongjie.app>
2026-09-22 12:41:23 +0000

    下午8:42：租客對話入口改成發送訊息

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1130";
-const BUILD = "20260922-0615";
+const CACHE = "tongjie-app-v1131";
+const BUILD = "20260922-0616";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
