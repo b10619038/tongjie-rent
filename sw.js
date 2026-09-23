d396ada0f447a72b2dc3ce178d7ae50d2dd8bf49
tongjie <b10619038@tongjie.app>
2026-09-23 15:41:03 +0000

    晚上11:48：日曆文字跟著格子縮放

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1248";
-const BUILD = "20260923-0733";
+const CACHE = "tongjie-app-v1249";
+const BUILD = "20260923-0734";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
