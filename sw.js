eb19d686e8a836efc4f08b56eef4f62c747e0227
tongjie <b10619038@tongjie.app>
2026-09-23 16:19:39 +0000

    晚上1:06：繳費切換列置中、底色改灰

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1263";
-const BUILD = "20260924-0748";
+const CACHE = "tongjie-app-v1264";
+const BUILD = "20260924-0749";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
