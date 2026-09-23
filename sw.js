25522b8913aa708214eb48b9d5208723352ab10b
tongjie <b10619038@tongjie.app>
2026-09-23 16:27:42 +0000

    晚上1:32：日曆租金改成紅點

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1269";
-const BUILD = "20260924-0754";
+const CACHE = "tongjie-app-v1270";
+const BUILD = "20260924-0755";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
