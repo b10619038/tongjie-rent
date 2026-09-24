c2974f649295943a6326442b0b8345003dfd53be
tongjie <b10619038@tongjie.app>
2026-09-24 19:44:42 +0000

    凌晨03:45：綁定 LINE 圖示改成三點對話框

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1424";
-const BUILD = "20260925-0909";
+const CACHE = "tongjie-app-v1425";
+const BUILD = "20260925-0910";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
