0e4578e6df9b209f549bdbf332eb83a5e744b822
tongjie <b10619038@tongjie.app>
2026-09-24 18:41:03 +0000

    凌晨02:41：手機後台選單一次只顯示四個

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1409";
-const BUILD = "20260925-0894";
+const CACHE = "tongjie-app-v1410";
+const BUILD = "20260925-0895";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
