d5494dc79e808b07b11f837861b3224c20e91177
tongjie <b10619038@tongjie.app>
2026-09-24 19:12:49 +0000

    凌晨03:13：後台橢圓選單底色改成 90%

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1414";
-const BUILD = "20260925-0899";
+const CACHE = "tongjie-app-v1415";
+const BUILD = "20260925-0900";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
