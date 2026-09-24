eb8b6eb6f684b70a239e6eee200409ecbcef650f
tongjie <b10619038@tongjie.app>
2026-09-24 09:03:45 +0000

    下午17:05：租客通知名稱改成兩欄標籤

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1330";
-const BUILD = "20260924-0815";
+const CACHE = "tongjie-app-v1331";
+const BUILD = "20260924-0816";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
