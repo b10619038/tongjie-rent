8bd4262d4cba31df70003b22bbfd7d24e5c7f573
tongjie <b10619038@tongjie.app>
2026-09-24 15:34:19 +0000

    晚上23:34：7021、7022、7023 平面圖樓層改成 2F

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1378";
-const BUILD = "20260924-0863";
+const CACHE = "tongjie-app-v1379";
+const BUILD = "20260924-0864";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
