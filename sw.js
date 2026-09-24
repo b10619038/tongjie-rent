0e85263f55ec737ae06292f9fff0172ef622fde2
tongjie <b10619038@tongjie.app>
2026-09-24 19:35:52 +0000

    凌晨03:36：合約標題回到中間

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1421";
-const BUILD = "20260925-0906";
+const CACHE = "tongjie-app-v1422";
+const BUILD = "20260925-0907";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
