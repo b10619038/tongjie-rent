3ed34e9e7d25dec7c96f4933e2b740d6cdbbe99d
tongjie <b10619038@tongjie.app>
2026-09-24 18:05:37 +0000

    凌晨02:04：資產平面圖點牛1會縮放並顯示牛1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1406";
-const BUILD = "20260925-0891";
+const CACHE = "tongjie-app-v1407";
+const BUILD = "20260925-0892";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
