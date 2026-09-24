3add82d893732a01e0883347a5bd805316bcf928
tongjie <b10619038@tongjie.app>
2026-09-24 02:28:24 +0000

    上午10:32：繳費總表標題改回合約

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1284";
-const BUILD = "20260924-0769";
+const CACHE = "tongjie-app-v1285";
+const BUILD = "20260924-0770";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
