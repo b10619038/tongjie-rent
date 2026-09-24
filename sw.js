49e9e2020ddc4bee1e1b2377e0ee7b2e5ec2a764
tongjie <b10619038@tongjie.app>
2026-09-24 19:34:30 +0000

    凌晨03:35：繳費狀態改到租金金額上面

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1420";
-const BUILD = "20260925-0905";
+const CACHE = "tongjie-app-v1421";
+const BUILD = "20260925-0906";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
