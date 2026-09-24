1d7510cbf9bb98e48c840201bab194f69fe7b9c6
tongjie <b10619038@tongjie.app>
2026-09-24 15:44:56 +0000

    晚上23:46：7623 平面圖縮到 7622 左邊那間

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1384";
-const BUILD = "20260924-0869";
+const CACHE = "tongjie-app-v1385";
+const BUILD = "20260924-0870";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
