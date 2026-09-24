70e95654195ee6225ab8be2de47360e5ff2ed51f
tongjie <b10619038@tongjie.app>
2026-09-24 19:47:35 +0000

    凌晨03:47：周邊景點圖示改成地圖釘

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1426";
-const BUILD = "20260925-0911";
+const CACHE = "tongjie-app-v1427";
+const BUILD = "20260925-0912";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
