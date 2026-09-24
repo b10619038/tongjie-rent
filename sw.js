2da55361c5f537f88b02c2966c039ff96f0c1a09
tongjie <b10619038@tongjie.app>
2026-09-24 19:02:54 +0000

    凌晨03:03：租約圖示改成簽名合約

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1411";
-const BUILD = "20260925-0896";
+const CACHE = "tongjie-app-v1412";
+const BUILD = "20260925-0897";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
