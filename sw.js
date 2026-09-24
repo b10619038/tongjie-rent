eb1e1a228bf5f79fc76bd8111fb26cf39680d46c
tongjie <b10619038@tongjie.app>
2026-09-24 12:49:09 +0000

    晚上20:48：租客名單房號和名字左邊切齊

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1348";
-const BUILD = "20260924-0833";
+const CACHE = "tongjie-app-v1349";
+const BUILD = "20260924-0834";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
