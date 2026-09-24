0cc209305b92ed91b9a0d6cb387c9bdfec261227
tongjie <b10619038@tongjie.app>
2026-09-24 07:49:17 +0000

    下午15:50：前往續約按鈕拉長

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1305";
-const BUILD = "20260924-0790";
+const CACHE = "tongjie-app-v1306";
+const BUILD = "20260924-0791";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
