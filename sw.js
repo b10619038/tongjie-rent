661a279299a0002da334c094d590d7fc010dd1c6
tongjie <b10619038@tongjie.app>
2026-09-24 16:57:05 +0000

    凌晨00:57：後台選單字放大

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1394";
-const BUILD = "20260925-0879";
+const CACHE = "tongjie-app-v1395";
+const BUILD = "20260925-0880";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
