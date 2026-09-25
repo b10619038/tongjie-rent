1fcfad7a5f188404f3e1ac369e113042e3b16fca
tongjie <b10619038@tongjie.app>
2026-09-25 06:30:30 +0000

    下午2:32：7231 的 10 月租金不算已繳，拿掉實繳日和印章

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1454";
-const BUILD = "20260925-0939";
+const CACHE = "tongjie-app-v1455";
+const BUILD = "20260925-0940";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
