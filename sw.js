14d8651e7526a5e8f9dae05a7bb080859c6b1d1f
tongjie <b10619038@tongjie.app>
2026-09-25 14:10:17 +0000

    晚上10:10：租約圖示線條加粗，和其他選單接近

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1500";
-const BUILD = "20260925-0985";
+const CACHE = "tongjie-app-v1501";
+const BUILD = "20260925-0986";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
