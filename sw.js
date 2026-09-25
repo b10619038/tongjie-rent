832cb9a8c4e714e5dcfd45bf5fee5205f9232e5f
tongjie <b10619038@tongjie.app>
2026-09-25 16:32:57 +0000

    凌晨12:33：後台選單圖示改成跟租客選單一樣的灰綠色

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1524";
-const BUILD = "20260926-1009";
+const CACHE = "tongjie-app-v1525";
+const BUILD = "20260926-1010";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
