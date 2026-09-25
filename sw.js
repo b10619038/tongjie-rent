ecf13ac1fbd73ee3817f9996be715d63d6258395
tongjie <b10619038@tongjie.app>
2026-09-25 16:55:37 +0000

    凌晨12:55：後台選中圖示跟租客一樣從中間放大

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1532";
-const BUILD = "20260926-1017";
+const CACHE = "tongjie-app-v1533";
+const BUILD = "20260926-1018";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
