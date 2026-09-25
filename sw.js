86c00d81c7290d9d8c755d0af36e51b26b06e1a5
tongjie <b10619038@tongjie.app>
2026-09-25 07:51:50 +0000

    下午3:52：選房拿掉不選，改說明小黃點是有陽台

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1471";
-const BUILD = "20260925-0956";
+const CACHE = "tongjie-app-v1472";
+const BUILD = "20260925-0957";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
