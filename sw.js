1af5f1155ee28b05ef464afae52b4dbd3bde37af
tongjie <b10619038@tongjie.app>
2026-09-25 17:18:17 +0000

    凌晨1:18：後台公告圖示改成擴音器

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1542";
-const BUILD = "20260926-1027";
+const CACHE = "tongjie-app-v1543";
+const BUILD = "20260926-1028";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
