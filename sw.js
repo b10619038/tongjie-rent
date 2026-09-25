ff1a7b6d7d4921f72deafee2b3b87317ea7b15f0
tongjie <b10619038@tongjie.app>
2026-09-25 08:37:45 +0000

    下午4:35：我的房間圖卡電腦也固定成跟手機一樣的淺灰

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1482";
-const BUILD = "20260925-0967";
+const CACHE = "tongjie-app-v1483";
+const BUILD = "20260925-0968";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
