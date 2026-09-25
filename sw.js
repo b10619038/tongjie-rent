9ba7b9bfe083b60b9ee1225591017b85679dc517
tongjie <b10619038@tongjie.app>
2026-09-25 16:36:47 +0000

    凌晨12:37：後台助手選單換成新的日曆圖示

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1526";
-const BUILD = "20260926-1011";
+const CACHE = "tongjie-app-v1527";
+const BUILD = "20260926-1012";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
