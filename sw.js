e815a8b653a0a4f2b11ff8f05e77e0ecb4fc614c
tongjie <b10619038@tongjie.app>
2026-09-25 17:17:16 +0000

    凌晨1:17：後台租客圖示改成單人

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1541";
-const BUILD = "20260926-1026";
+const CACHE = "tongjie-app-v1542";
+const BUILD = "20260926-1027";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
