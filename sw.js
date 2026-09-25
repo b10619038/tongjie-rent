764506b380746c684fc0208ad4f12fc9a6a3ec69
tongjie <b10619038@tongjie.app>
2026-09-25 08:09:03 +0000

    下午4:09：底部選單透明度改成 95%

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1476";
-const BUILD = "20260925-0961";
+const CACHE = "tongjie-app-v1477";
+const BUILD = "20260925-0962";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
