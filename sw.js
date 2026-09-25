ccdbb64cf586db622b4022a818a4ec2d93d158a6
tongjie <b10619038@tongjie.app>
2026-09-25 04:47:50 +0000

    中午12:48：租客資料頁返回的下方間距改成和上方一樣

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1446";
-const BUILD = "20260925-0931";
+const CACHE = "tongjie-app-v1447";
+const BUILD = "20260925-0932";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
