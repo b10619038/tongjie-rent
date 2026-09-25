ae77425d0975f049fd0b90fd63879aa1c1a30964
tongjie <b10619038@tongjie.app>
2026-09-25 16:44:01 +0000

    凌晨12:43：後台選單橢圓底改成跟租客選單一樣高

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1528";
-const BUILD = "20260926-1013";
+const CACHE = "tongjie-app-v1529";
+const BUILD = "20260926-1014";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
