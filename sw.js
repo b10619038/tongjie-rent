cda8820a3327caa8396df915f78c7317ac5c8fb7
tongjie <b10619038@tongjie.app>
2026-09-25 03:55:39 +0000

    中午11:56：訊息泡泡改成 LINE 的尾巴形狀

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1434";
-const BUILD = "20260925-0919";
+const CACHE = "tongjie-app-v1435";
+const BUILD = "20260925-0920";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
