86e6a4cc5a2421e9f82879cc756625651e16dd6c
tongjie <b10619038@tongjie.app>
2026-09-25 03:57:06 +0000

    中午11:58：管理員訊息泡泡也加上 LINE 尾巴

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1435";
-const BUILD = "20260925-0920";
+const CACHE = "tongjie-app-v1436";
+const BUILD = "20260925-0921";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
