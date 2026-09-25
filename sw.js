18bb8573d688bd9caa6977557db2a06a79c53a91
tongjie <b10619038@tongjie.app>
2026-09-25 15:10:37 +0000

    晚上11:10：資產圖示線條加粗，租客圖示縮小

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1514";
-const BUILD = "20260925-0999";
+const CACHE = "tongjie-app-v1515";
+const BUILD = "20260925-1000";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
