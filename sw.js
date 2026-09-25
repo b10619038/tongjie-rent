03ffb2f40f1cc4892da06a8451c49349a9beb79e
tongjie <b10619038@tongjie.app>
2026-09-25 15:21:59 +0000

    晚上11:22：助手日曆圖示線條加粗

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1519";
-const BUILD = "20260925-1004";
+const CACHE = "tongjie-app-v1520";
+const BUILD = "20260925-1005";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
