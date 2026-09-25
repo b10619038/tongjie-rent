dab166fcfc74c68bbf8818f520084b46a526f827
tongjie <b10619038@tongjie.app>
2026-09-25 08:40:34 +0000

    下午4:39：有陽台的黃點跟房號旁的黃點對齊

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1483";
-const BUILD = "20260925-0968";
+const CACHE = "tongjie-app-v1484";
+const BUILD = "20260925-0969";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
