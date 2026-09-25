5450da01c0a415e818fe4e9eb3a2b9e722d24045
tongjie <b10619038@tongjie.app>
2026-09-25 16:50:19 +0000

    凌晨12:50：後台選單圖示高度跟租客選單對齊

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1530";
-const BUILD = "20260926-1015";
+const CACHE = "tongjie-app-v1531";
+const BUILD = "20260926-1016";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
