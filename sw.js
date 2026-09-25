6dcf83b75ba43b49ac43587c1b7c3ef9d7f4eeb4
tongjie <b10619038@tongjie.app>
2026-09-25 04:43:02 +0000

    中午12:43：租客資料頁的返回上下間距收緊

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1443";
-const BUILD = "20260925-0928";
+const CACHE = "tongjie-app-v1444";
+const BUILD = "20260925-0929";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
