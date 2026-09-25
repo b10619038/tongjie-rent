796b2e397c8dba363fdeee4aa034a0ea52c4dce8
tongjie <b10619038@tongjie.app>
2026-09-25 08:23:48 +0000

    下午4:23：報修工具箱改成跟其他圖示一樣的灰綠色

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1480";
-const BUILD = "20260925-0965";
+const CACHE = "tongjie-app-v1481";
+const BUILD = "20260925-0966";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
