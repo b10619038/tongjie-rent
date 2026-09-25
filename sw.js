a401d5488f119791210bc45f659302bf1587e302
tongjie <b10619038@tongjie.app>
2026-09-25 17:20:00 +0000

    凌晨1:20：後台資料圖示改成資料夾

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1543";
-const BUILD = "20260926-1028";
+const CACHE = "tongjie-app-v1544";
+const BUILD = "20260926-1029";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
