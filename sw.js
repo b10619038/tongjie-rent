95e9632da6c38176fdf9f8052151074b40e42f26
tongjie <b10619038@tongjie.app>
2026-09-25 16:31:19 +0000

    凌晨12:31：後台資產選單換成房子圖示

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1523";
-const BUILD = "20260926-1008";
+const CACHE = "tongjie-app-v1524";
+const BUILD = "20260926-1009";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
