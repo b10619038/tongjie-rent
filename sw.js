482bd0713767f47c5293377f49c72b26d3707833
tongjie <b10619038@tongjie.app>
2026-09-25 17:06:47 +0000

    凌晨1:07：助手日曆圖示線條改回原本

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1536";
-const BUILD = "20260926-1021";
+const CACHE = "tongjie-app-v1537";
+const BUILD = "20260926-1022";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
