e4ef21e4b48742556213273a3fe8a1673e78b289
tongjie <b10619038@tongjie.app>
2026-09-25 15:19:55 +0000

    晚上11:19：後台選單圖示改成跟租客選單一樣大

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1518";
-const BUILD = "20260925-1003";
+const CACHE = "tongjie-app-v1519";
+const BUILD = "20260925-1004";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
