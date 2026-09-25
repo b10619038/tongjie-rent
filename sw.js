278c86c8a20be4f84f8b4824ca3537c7abe0a86a
tongjie <b10619038@tongjie.app>
2026-09-25 07:21:40 +0000

    下午3:22：選房平面圖小圖改到房號和金額右邊

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1463";
-const BUILD = "20260925-0948";
+const CACHE = "tongjie-app-v1464";
+const BUILD = "20260925-0949";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
