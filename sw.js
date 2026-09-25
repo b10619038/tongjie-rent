3b73f75c7612e184fbe053ce2f3c93f4e32389e7
tongjie <b10619038@tongjie.app>
2026-09-25 15:33:52 +0000

    晚上11:34：後台選單圖示高度改成跟租客選單一樣

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1520";
-const BUILD = "20260925-1005";
+const CACHE = "tongjie-app-v1521";
+const BUILD = "20260925-1006";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
