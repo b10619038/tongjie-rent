41cfa24bfc8942486a51fbf0c7345bb5d567ac7f
tongjie <b10619038@tongjie.app>
2026-09-25 08:54:39 +0000

    下午4:54：極黑模式的聊天室改成深灰

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1486";
-const BUILD = "20260925-0971";
+const CACHE = "tongjie-app-v1487";
+const BUILD = "20260925-0972";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
