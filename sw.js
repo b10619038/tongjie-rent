3b9ee9fd801f0e616ebdc86ece21beed4f584d96
tongjie <b10619038@tongjie.app>
2026-09-25 17:26:53 +0000

    凌晨1:27：後台飲食圖示改成叉子和湯匙

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1546";
-const BUILD = "20260926-1031";
+const CACHE = "tongjie-app-v1547";
+const BUILD = "20260926-1032";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
