1621a390adf13b72683fa873d631f142dceebcfe
tongjie <b10619038@tongjie.app>
2026-09-25 07:27:57 +0000

    下午3:28：選房小圖換成新圖，上下對齊，左右文字不動

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1465";
-const BUILD = "20260925-0950";
+const CACHE = "tongjie-app-v1466";
+const BUILD = "20260925-0951";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
