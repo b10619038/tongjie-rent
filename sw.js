238eb2c30c4070cb48d8772582858af9c1ba2bcf
tongjie <b10619038@tongjie.app>
2026-09-26 04:38:04 +0000

    中午12:38：報修進度機車圖縮小一半

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1563";
-const BUILD = "20260926-1048";
+const CACHE = "tongjie-app-v1564";
+const BUILD = "20260926-1049";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
