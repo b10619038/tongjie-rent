964b7d88e0bdca218f904f75e6586be2d1f6b901
tongjie <b10619038@tongjie.app>
2026-09-26 09:29:46 +0000

    下午17:29：完成簽約跟姓名同一行靠右

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1577";
-const BUILD = "20260926-1062";
+const CACHE = "tongjie-app-v1578";
+const BUILD = "20260926-1063";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
