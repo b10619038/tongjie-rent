e5cf73ddaa7107e4efcebede4c6f0895635f07c8
tongjie <b10619038@tongjie.app>
2026-09-26 13:35:10 +0000

    下午21:34：一般汽車改成側視小車造型

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1598";
-const BUILD = "20260926-1083";
+const CACHE = "tongjie-app-v1599";
+const BUILD = "20260926-1084";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
