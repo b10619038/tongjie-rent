cc8513267a4a3385d44a59df7dec010b15a89896
tongjie <b10619038@tongjie.app>
2026-09-26 08:23:04 +0000

    下午16:23：其他車輛改成90%透明度

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1571";
-const BUILD = "20260926-1056";
+const CACHE = "tongjie-app-v1572";
+const BUILD = "20260926-1057";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
