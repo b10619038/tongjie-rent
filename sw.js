814eb2848a32f53536b56500bdd482ac4a288dff
tongjie <b10619038@tongjie.app>
2026-09-26 13:46:30 +0000

    下午21:46：貨車改成鏡射後的小貨車

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1599";
-const BUILD = "20260926-1084";
+const CACHE = "tongjie-app-v1600";
+const BUILD = "20260926-1085";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
