8f3f8f07ecf1fae13ffedd64e4301993a478cb3e
tongjie <b10619038@tongjie.app>
2026-09-26 09:56:04 +0000

    下午17:55：報修進度的車子改成淡灰色

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1583";
-const BUILD = "20260926-1068";
+const CACHE = "tongjie-app-v1584";
+const BUILD = "20260926-1069";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
