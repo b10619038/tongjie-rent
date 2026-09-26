dc3b0d3818f7f0353f8540ea21ee60b81821dc8c
tongjie <b10619038@tongjie.app>
2026-09-26 08:10:47 +0000

    下午16:10：紅綠燈改成橫向，桿子變細

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1566";
-const BUILD = "20260926-1051";
+const CACHE = "tongjie-app-v1567";
+const BUILD = "20260926-1052";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
