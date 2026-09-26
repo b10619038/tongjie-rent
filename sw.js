24ad1e8922f238b85ba1a8d5e096f905582825f6
tongjie <b10619038@tongjie.app>
2026-09-26 08:12:34 +0000

    下午16:13：橫向紅綠燈縮成正常長度

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1567";
-const BUILD = "20260926-1052";
+const CACHE = "tongjie-app-v1568";
+const BUILD = "20260926-1053";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
