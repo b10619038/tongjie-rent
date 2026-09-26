dd26bdd95147978e4070037ec7311de8b2567ad9
tongjie <b10619038@tongjie.app>
2026-09-26 13:56:22 +0000

    下午21:56：卡車往下貼齊路面

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1602";
-const BUILD = "20260926-1087";
+const CACHE = "tongjie-app-v1603";
+const BUILD = "20260926-1088";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
