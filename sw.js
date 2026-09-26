30ef5ea818c21733338a35e46150a54f56c1572a
tongjie <b10619038@tongjie.app>
2026-09-26 10:59:57 +0000

    下午18:59：樹改成圓冠有分枝的形狀

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1595";
-const BUILD = "20260926-1080";
+const CACHE = "tongjie-app-v1596";
+const BUILD = "20260926-1081";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
