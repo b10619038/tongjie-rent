10a81dd46510f5ea7253a074701d7ce5420b8fcd
tongjie <b10619038@tongjie.app>
2026-09-26 03:22:01 +0000

    上午11:22：報修一次選多張照片會全部留下

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "tongjie-app-v1552";
-const BUILD = "20260926-1037";
+const CACHE = "tongjie-app-v1553";
+const BUILD = "20260926-1038";
 const FILES = ["/", "/index.html", "/app.css", "/app.js", "/work-scroll.css", "/work-enhance.js", "/manifest.json", "/icon-192.png", "/icon-512.png", "/icon-maskable-512.png"];
 self.addEventListener("install", e => {
   self.skipWaiting();
